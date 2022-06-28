e865b88719ea4a0f5a94a7c1261a5956c7b6ee6c
YuriiPohorilets <102477210+YuriiPohorilets@users.noreply.github.com>
2022-06-28 16:32:39 +0000

    Deploying to gh-pages from @ YuriiPohorilets/icecream-project-01@e9e64846558b24d3b0bc5ff28264349e5f9e1a32 🚀

---
+++ index.a7aae511.js
@@ -0,0 +1,2 @@
+function changeScrollButtonVisibility(){var t=document.querySelector(".scroll-to-top");window.pageYOffset>60?t.classList.remove("js-transparent"):t.classList.add("js-transparent")}window.onscroll=function(){return changeScrollButtonVisibility()};
+//# sourceMappingURL=index.a7aae511.js.map
