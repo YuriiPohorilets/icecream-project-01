0d4e451a6db2daee27151508b03b743b40c67499
YuriiPohorilets <pohorilets.y@gmail.com>
2022-06-29 22:37:09 +0300

    stop scroll open menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,21 +2,24 @@
   const mobileMenu = document.querySelector('.js-menu');
   const openMenuBtn = document.querySelector('.js-menu');
   const closeMenuBtn = document.querySelector('.js-menu');
+  // const closeMenuLink = document.querySelector('.js-link');
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    document.body.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    // const scrollLockMethod = !isMenuOpen
+    //   ? 'disableBodyScroll'
+    //   : 'enableBodyScroll';
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  // closeMenuLink.addEventListener('click', toggleMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
